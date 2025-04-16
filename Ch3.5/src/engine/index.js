363c60c600f1d1269bdb991d6eb240ddf9aa2c7c
SheepCommander <66646514+SheepCommander@users.noreply.github.com>
2025-04-16 12:05:18 -0700

    Ch3.5 (and chapter 3) done!

---
+++ Ch3.5/src/engine/index.js
@@ -0,0 +1,28 @@
+// local to this file only
+import * as glSys from "./core/gl.js";
+import * as vertexBuffer from "./core/vertex_buffer.js";
+import * as shaderResources from "./core/shader_resources.js";
+// general engine utilities
+function init(htmlCanvasID) {
+    glSys.init(htmlCanvasID);
+    vertexBuffer.init();
+    shaderResources.init();
+}
+
+function clearCanvas(color) {
+    let gl = glSys.get();
+    gl.clearColor(color[0], color[1], color[2], color[3]);
+    gl.clear(gl.COLOR_BUFFER_BIT); // clear to the color set
+}
+
+// general utilities
+import Camera from "./camera.js";
+import Transform from "./transform.js";
+import Renderable from "./renderable.js";
+
+export default {
+    // Util classes
+    Camera, Transform, Renderable,
+    // functions
+    init, clearCanvas
+}
---
+++ Ch3.5/src/my_game/my_game.js
@@ -0,0 +1,65 @@
+// import from engine/index.js for all engine symbols
+import engine from "../engine/index.js";
+
+class MyGame {
+    constructor(htmlCanvasID) {
+        // Step A: Initialize the game engine
+        engine.init(htmlCanvasID);
+        // Step B: Setup the camera
+        this.mCamera = new engine.Camera(
+            vec2.fromValues(20, 60), // center of the WC
+            20, // width of WC
+            [20, 40, 600, 300] // viewport:orgX, orgY, W, H
+        );
+        // Step C: Create the Renderable objects:
+        this.mBlueSq = new engine.Renderable();
+        this.mBlueSq.setColor([0.25, 0.25, 0.95, 1]);
+        this.mRedSq = new engine.Renderable();
+        this.mRedSq.setColor([1, 0.25, 0.25, 1]);
+        this.mTLSq = new engine.Renderable();
+        this.mTLSq.setColor([0.9, 0.1, 0.1, 1]);
+        this.mTRSq = new engine.Renderable();
+        this.mTRSq.setColor([0.1, 0.9, 0.1, 1]);
+        this.mBRSq = new engine.Renderable();
+        this.mBRSq.setColor([0.1, 0.1, 0.9, 1]);
+        this.mBLSq = new engine.Renderable();
+        this.mBLSq.setColor([0.1, 0.1, 0.1, 1]);
+        // Step D: Clear the canvas
+        engine.clearCanvas([0.9, 0.9, 0.9, 1]); // Clear the canvas
+        // Step E: Starts the drawing by activating the camera
+        this.mCamera.setViewAndCameraMatrix();
+        // Step F: Draw the blue square
+        // Center Blue, slightly rotated square
+        this.mBlueSq.getXform().setPosition(20, 60);
+        this.mBlueSq.getXform().setRotationInRad(0.2); // In Radians
+        this.mBlueSq.getXform().setSize(5, 5);
+        this.mBlueSq.draw(this.mCamera);
+        // Step G: Draw the center and the corner squares
+        // center red square
+        this.mRedSq.getXform().setPosition(20, 60);
+        this.mRedSq.getXform().setSize(2, 2);
+        this.mRedSq.draw(this.mCamera);
+        // top left
+        this.mTLSq.getXform().setPosition(10, 65);
+        this.mTLSq.draw(this.mCamera);
+        // top right
+        this.mTRSq.getXform().setPosition(30, 65);
+        this.mTRSq.draw(this.mCamera);
+        // bottom right
+        this.mBRSq.getXform().setPosition(30, 55);
+        this.mBRSq.draw(this.mCamera);
+        // bottom left
+        this.mBLSq.getXform().setPosition(10, 55);
+        this.mBLSq.draw(this.mCamera);
+    }
+}
+window.onload = function () {
+    new MyGame('GLCanvas');
+}
+/**
+a. Center: (20,60)
+b. Top-lep corner: (10, 65)
+c. Top-right corner: (30, 65)
+d. Bohom-right corner: (30, 55)
+e. Bohom-lep corner: (10, 55)
+ */
