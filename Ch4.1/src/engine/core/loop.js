01ba182b02eb11e9c6ece0bfe0dc9e4eb833e8f3
SheepCommander <66646514+SheepCommander@users.noreply.github.com>
2025-04-16 12:31:22 -0700

    4.1 finished

---
+++ Ch4.1/src/engine/core/loop.js
@@ -0,0 +1,59 @@
+"use strict"
+const kUPS = 60; // Updates per second
+const kMPF = 1000 / kUPS; // Milliseconds per update.
+// Variables for timing gameloop.
+let mPrevTime;
+let mLagTime;
+// The current loop state (running or should stop)
+let mLoopRunning = false;
+let mCurrentScene = null;
+let mFrameID = -1;
+
+function loopOnce() {
+    if (mLoopRunning) {
+        // Step A: set up for next call to LoopOnce
+        mFrameID = requestAnimationFrame(loopOnce);
+        // Step B: now let's draw
+        // draw() MUST be called before update()
+        // as update() may stop the loop!
+        mCurrentScene.draw();
+        // Step C: compute time elapsed since last loopOnce was executed
+        let currentTime = performance.now();
+        let elapsedTime = currentTime - mPrevTime;
+        mPrevTime = currentTime;
+        mLagTime += elapsedTime;
+        // Step D: update the game the appropriate number of times.
+        // Update only every kMPF (1/60 of a second)
+        // If lag larger then update frames, update until caught up.
+        while ((mLagTime >= kMPF) && mLoopRunning) {
+            mCurrentScene.update();
+            mLagTime -= kMPF;
+        }
+    }
+}
+
+/**
+ * 
+ * @param {*} scene the loopOnce function
+ */
+function start(scene) {
+    if (mLoopRunning) {
+        throw new Error("loop already running")
+    }
+    mCurrentScene = scene;
+    mCurrentScene.init();
+    mPrevTime = performance.now();
+    mLagTime = 0.0;
+    mLoopRunning = true;
+    mFrameID = requestAnimationFrame(loopOnce);
+}
+
+/**
+ * Stops the loop by setting mLoopRunning to false and cancels last requested animation frame.
+ */
+function stop() {
+    mLoopRunning = false;
+    // make sure no more animation frames
+    cancelAnimationFrame(mFrameID);
+}
+export { start, stop }
---
+++ Ch4.1/src/my_game/my_game.js
@@ -0,0 +1,76 @@
+// Accessing engine internal is not ideal,
+// this must be resolved! (later)
+import * as loop from "../engine/core/loop.js";
+// import from engine/index.js for all engine symbols
+import engine from "../engine/index.js";
+
+class MyGame {
+    constructor() {
+        // variables for the squares
+        this.mWhiteSq = null; // these are the Renderable objects
+        this.mRedSq = null;
+        // The camera to view the scene
+        this.mCamera = null;
+    }
+    init() {
+        // Step A: set up the cameras
+        this.mCamera = new engine.Camera(
+            vec2.fromValues(20, 60), // position of the camera
+            20, // width of camera
+            [20, 40, 600, 300] // viewport (orgX, orgY, width, height)
+        );
+        this.mCamera.setBackgroundColor([0.8, 0.8, 0.8, 1]);
+        // sets the background to gray
+        // Step B: Create the Renderable objects:
+        this.mWhiteSq = new engine.Renderable();
+        this.mWhiteSq.setColor([1, 1, 1, 1]);
+        this.mRedSq = new engine.Renderable();
+        this.mRedSq.setColor([1, 0, 0, 1]);
+        // Step C: Init the white Renderable: centered, 5x5, rotated
+        this.mWhiteSq.getXform().setPosition(20, 60);
+        this.mWhiteSq.getXform().setRotationInRad(0.2); // In Radians
+        this.mWhiteSq.getXform().setSize(5, 5);
+
+        // Step D: Initialize the red Renderable object: centered 2x2
+        this.mRedSq.getXform().setPosition(20, 60);
+        this.mRedSq.getXform().setSize(2, 2);
+    }
+    draw() {
+        // Step A: clear the canvas
+        engine.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
+        // Step B: Activate the drawing Camera
+        this.mCamera.setViewAndCameraMatrix();
+        // Step C: Activate the white shader to draw
+        this.mWhiteSq.draw(this.mCamera);
+        // Step D: Activate the red shader to draw
+        this.mRedSq.draw(this.mCamera);
+    }
+    update() {
+        // Simple game: move the white square and pulse the red
+        let whiteXform = this.mWhiteSq.getXform();
+        let deltaX = 0.05;
+        // Step A: Rotate the white square
+        if (whiteXform.getXPos() > 30) // the right-bound of the window
+            whiteXform.setPosition(10, 60);
+        whiteXform.incXPosBy(deltaX);
+        whiteXform.incRotationByDegree(1);
+        // Step B: pulse the red square
+        let redXform = this.mRedSq.getXform();
+        if (redXform.getWidth() > 5)
+            redXform.setSize(2, 2);
+        redXform.incSizeBy(0.05);
+    }
+}
+window.onload = function () {
+    engine.init("GLCanvas");
+    let myGame = new MyGame();
+    // new begins the game
+    loop.start(myGame);
+}
+/**
+a. Center: (20,60)
+b. Top-lep corner: (10, 65)
+c. Top-right corner: (30, 65)
+d. Bohom-right corner: (30, 55)
+e. Bohom-lep corner: (10, 55)
+ */
